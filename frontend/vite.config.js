82d434a6bf161ea49e7a84c0a6a87887fe2fe4ef
Ayobami6 <ayobamidele006@gmail.com>
2026-05-18 22:17:19 +0100

    feat: configure API proxying for local development and Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://interview-pillot-alb-1300503305.us-east-1.elb.amazonaws.com',
+        changeOrigin: true
+      }
+    }
+  }
 })
